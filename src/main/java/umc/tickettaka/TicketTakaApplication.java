76f0984fc881e29fc50a63be21b78e33e1fe1639
yuuddin <jindf51@naver.com>
2024-01-08 21:15:29 +0900

    [CHORE] 프로젝트 기본 세팅 #1

---
+++ src/main/java/umc/tickettaka/TicketTakaApplication.java
@@ -0,0 +1,13 @@
+package umc.tickettaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketTakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketTakaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/umc/tickettaka/TicketTakaApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.tickettaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketTakaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
