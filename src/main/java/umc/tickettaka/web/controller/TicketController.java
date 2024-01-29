6adace662f8fd144490266b9baca67ec8659bc98
Kim Doo Hyeon <engus525@naver.com>
2024-01-29 22:45:48 +0900

    [REFACTOR] Swagger @Operation, @Parameters 적용 #128

--- src/main/java/umc/tickettaka/web/controller/TicketController.java
+++ src/main/java/umc/tickettaka/web/controller/TicketController.java
@@ -1,5 +1,8 @@
 package umc.tickettaka.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -29,23 +32,38 @@ public class TicketController {
     private final MemberCommandService memberCommandService;
 
     @GetMapping
+    @Operation(summary = "Timeline별로 Ticket 조회")
+    @Parameters({
+        @Parameter(name = "teamId", description = "팀 아이디 : Path Variable"),
+        @Parameter(name = "timelineId", description = "타임라인 아이디 : Path Variable"),
+        @Parameter(name = "status", description = "티켓 상태 : todo, inprogress, done 셋 중 하나입니다.")
+    })
     public ApiResponse<TicketResponseDto.ShowAllTicketListDto> showAllTickets(
         @AuthUser Member member,
         @PathVariable(name = "teamId") Long teamId,
         @PathVariable(name = "timelineId") Long timelineId,
-        @RequestParam(name ="status")String status
+        @RequestParam(name = "status") String status
     ) {
         return ApiResponse.onSuccess(ticketQueryService.getShowAllTicketListDto(member, teamId, timelineId, status));
     }
 
     //todo team controller로 이동
     @GetMapping("/member-info")
+    @Operation(summary = "Ticket 생성창 조회")
+    @Parameters({
+        @Parameter(name = "teamId", description = "팀 아이디 : Path Variable")
+    })
     public ApiResponse<CommonMemberDto.ShowMemberProfileListDto> createTicketPage(
         @PathVariable(name = "teamId") Long teamId
     ) {
         return ApiResponse.onSuccess(memberCommandService.getCommonMemberDto(teamId));
     }
+
     @PostMapping
+    @Operation(summary = "Ticket 생성 요청")
+    @Parameters({
+        @Parameter(name = "timelineId", description = "타임라인 아이디 : Path Variable")
+    })
     public ApiResponse<TicketResponseDto.CreateTicketResultDto> createTicket(
         @PathVariable(name = "timelineId") Long timelineId,
         @RequestBody TicketRequestDto.CreateTicketDto request
@@ -55,6 +73,7 @@ public class TicketController {
     }
 
     @DeleteMapping
+    @Operation(summary = "Ticket 삭제")
     public ApiResponse<List<ShowTicketDto>> deleteTicket(
         @RequestBody TicketRequestDto.DeleteTicketDto request
     ) {
