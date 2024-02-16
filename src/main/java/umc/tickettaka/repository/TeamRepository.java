0ddd55202b29d6ef49125ea6ef41d83ababef490
Kim Doo Hyeon <engus525@naver.com>
2024-02-17 04:31:28 +0900

    [REFACTOR] Team Refactoring #170

--- src/main/java/umc/tickettaka/repository/TeamRepository.java
+++ src/main/java/umc/tickettaka/repository/TeamRepository.java
@@ -1,11 +1,22 @@
 package umc.tickettaka.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import umc.tickettaka.domain.Member;
+import umc.tickettaka.domain.Project;
 import umc.tickettaka.domain.Team;
 
 import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
     List<Team> findByMemberTeamListMember(Member member);
+
+    @Query("SELECT t FROM Team t "
+        + "LEFT JOIN t.projectList p "
+        + "LEFT JOIN p.timelineList tl "
+        + "WHERE tl.id = :timelineId and p = :project")
+    Optional<Team> findTeamByProjectAndTimeline(@Param("project") Project project, @Param("timelineId") Long timelineId);
+
 }
--- src/main/java/umc/tickettaka/service/TeamQueryService.java
+++ src/main/java/umc/tickettaka/service/TeamQueryService.java
@@ -3,11 +3,12 @@ package umc.tickettaka.service;
 import java.util.List;
 
 import umc.tickettaka.domain.Member;
+import umc.tickettaka.domain.Project;
 import umc.tickettaka.domain.Team;
-import java.util.Optional;
 
 public interface TeamQueryService {
     Team findTeam(Long id);
+    Team findTeamByProjectAndTimeline(Project project, Long timelineId);
     List<Team> findAll();
     List<Team> findTeamsByMember(Member member);
 }
